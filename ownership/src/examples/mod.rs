d24c1ee0c4041087fd3d0405e06d099e1cb7e336
omegga <whymagnet@gmail.com>
2020-08-09 19:42:18 +0200

    update ownership

--- ownership/src/examples/mod.rs
+++ ownership/src/examples/mod.rs
@@ -1,6 +1,8 @@
 mod filling_vectors_1;
 mod filling_vectors_2;
 mod filling_vectors_3;
+mod filling_vectors_4;
+mod filling_vectors_5;
 
 // These examples show the differents ways of mutating a vector passed
 // as an argument to a function
@@ -8,4 +10,6 @@ pub fn mutate_a_vector() {
     filling_vectors_1::example();
     filling_vectors_2::example();
     filling_vectors_3::example();
+    filling_vectors_4::example();
+    filling_vectors_5::example();
 }
